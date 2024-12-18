82add2954ff594e23b0bfba061c67718da585361
BlueM2ST <37814898+BlueM2ST@users.noreply.github.com>
2024-12-18 13:33:37 -0400

    added vector2d class

---
+++ vector2d.py
@@ -0,0 +1,13 @@
+
+
+class Vector2D():
+    def __init__(self, vector:tuple):
+        self.vector = vector
+        self.x = vector[0]
+        self.y = vector[1]
+    
+    def __repr__(self):
+        return self.vector
+
+    def __str__(self):
+        return str(self.vector)
