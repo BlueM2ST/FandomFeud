7043278ccc5829dee22188ff6a301f05e1b131a9
BlueM2ST <37814898+BlueM2ST@users.noreply.github.com>
2024-12-18 14:38:58 -0400

    only re-render font when text changes

--- textBox.py
+++ textBox.py
@@ -1,5 +1,5 @@
 
-from pygame import font
+from pygame import font, time
 from screenObject import ScreenObject
 
 font.init()
@@ -9,7 +9,7 @@ font = font.Font(None, 24)
 class TextBox(ScreenObject):
     def __init__(self, name:str, posX:int, posY:int, text:str="", center:bool=True, padding:int=0):
         super().__init__(name, posX, posY)
-        self.text = text
+        self.text = font.render(text, True, (0, 0, 0))
         self.center = center
         self.padding = padding
 
@@ -18,11 +18,10 @@ class TextBox(ScreenObject):
         # scale with screen size
         x, y, w, h = self.scale(screen, initX, initY)
         # if text, draw text on top of background
-        text = font.render(self.text, True, (0, 0, 0))
         textRect = (x, y)
         if self.center:
-            textRect = text.get_rect(center = (x + self.getParent().scaleSize.x /2, y + self.getParent().scaleSize.y /2))
-        screen.blit(text, (textRect[0] + self.padding, textRect[1] + self.padding))
+            textRect = self.text.get_rect(center = (x + self.getParent().scaleSize.x /2, y + self.getParent().scaleSize.y /2))
+        screen.blit(self.text, (textRect[0] + self.padding, textRect[1] + self.padding))
     
     def setText(self, text):
-        self.text = text
+        self.text = font.render(text, True, (0, 0, 0))
