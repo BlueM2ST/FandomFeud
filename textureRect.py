8bcbbbe855b8fef63ff849e156aeccd928505b92
BlueM2ST <37814898+BlueM2ST@users.noreply.github.com>
2024-12-18 00:31:21 -0400

    added rect to show texture

---
+++ textureRect.py
@@ -0,0 +1,22 @@
+
+
+from pygame import draw, image, transform
+from screenObject import ScreenObject
+
+
+class TextureRect(ScreenObject):
+    def __init__(self, name:str, posX:int, posY:int, width:int, height:int, imagePath:str):
+        self.name = name
+        self.posX = posX
+        self.posY = posY
+        self.width = width
+        self.height = height
+        self.image = image.load(imagePath).convert_alpha()
+        self.children = []
+        self.parent = None
+        self.nodes = []
+    
+    def draw(self, screen, initX, initY):
+        x, y, w, h = self.scale(screen, initX, initY)
+        image = transform.smoothscale(self.image, (w, h))
+        screen.blit(image, (x, y))
