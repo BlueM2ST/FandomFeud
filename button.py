9d451d26ffbed33e8efc80bfea04ab55ba95ab44
BlueM2ST <37814898+BlueM2ST@users.noreply.github.com>
2024-12-19 19:07:45 -0400

    button now inherits colorrect

--- button.py
+++ button.py
@@ -1,25 +1,17 @@
 
-from pygame import draw, MOUSEBUTTONDOWN
-from screenObject import ScreenObject
+from pygame import MOUSEBUTTONDOWN
+from colorRect import ColorRect
 from textBox import TextBox
 
 
-class Button(ScreenObject):
+class Button(ColorRect):
     def __init__(self, name:str, posX:int, posY:int, width:int, height:int, text:str="", color:tuple=(255, 255, 0), connection:list=[], disabled:bool=False, centerText:bool=True):
-        super().__init__(name, posX, posY)
-        self.size = self.toVector((width, height))
-        self.text = text
-        self.color = color
+        super().__init__(name, posX, posY, width, height, color)
         self.connection = connection
         self.connection.append(self)
         self.disabled = disabled
         self.centerText = centerText
-        if self.text:
-            self.addChild(TextBox(self.name+"text", 0, 0, self.text, center=centerText))
-
-
-    def draw(self, screen, initX:int, initY:int):
-        self.rect = draw.rect(screen, self.color, (self.scale(screen, initX, initY)), 0)
+        self.textBox = self.addChild(TextBox("textbox", 0, 0, text, center=centerText))
 
     def handleEvent(self, event):
         if event.type == MOUSEBUTTONDOWN and event.button == 1:
@@ -28,4 +20,7 @@ class Button(ScreenObject):
                     return self.connection
 
     def setText(self, text):
-        self.getChild(self.name + "text").setText(text)
+        self.textBox.setText(text)
+    
+    def getText(self):
+        return self.textBox.getText()
