a58dc6538054e7e0b518cce0902d04821d210679
BlueM2ST <37814898+BlueM2ST@users.noreply.github.com>
2024-12-19 18:01:14 -0400

    textedit now uses a colorrect and textbox for drawing

--- textEdit.py
+++ textEdit.py
@@ -1,28 +1,22 @@
 
 
 from pygame import draw, MOUSEBUTTONDOWN, KEYDOWN, K_BACKSPACE
+from screenObject import ScreenObject
+from colorRect import ColorRect
 from textBox import TextBox
 
 
-class TextEdit(TextBox):
+class TextEdit(ScreenObject):
     def __init__(self, name:str, posX:int, posY:int, width:int, height:int, text:str="", center:bool=False, padding:int=0):
         super().__init__(name, posX, posY)
         self.size = self.toVector((width, height))
-        self.scaleSize = self.toVector((0, 0))
         self.colorInactive = (100, 100, 100)
         self.colorActive = (150, 150, 150)
         self.colorCurrent = self.colorInactive
         self.active = False
-    
-    def draw(self, screen, initX:int, initY:int):
-        self.rect = draw.rect(screen, self.colorCurrent, (self.scale(screen, initX, initY)), 0)
-        # scale with screen size
-        x, y, w, h = self.scale(screen, initX, initY)
-        # if text, draw text on top of background
-        textRect = (x, y)
-        if self.center:
-            textRect = self.renderedText.get_rect(center = (x + self.getParent().scaleSize.x /2, y + self.getParent().scaleSize.y /2))
-        screen.blit(self.renderedText, (textRect[0] + self.padding, textRect[1] + self.padding))
+        self.text = text
+        self.rect = self.addChild(ColorRect(self.name + "rect", self.pos.x, self.pos.y, self.size.x, self.size.y, self.colorInactive))
+        self.textBox = self.addChild(TextBox(self.name + "text", self.pos.x, self.pos.y, self.text))
 
     def handleEvent(self, event):
         if event.type == MOUSEBUTTONDOWN:
@@ -38,6 +32,7 @@ class TextEdit(TextBox):
                     self.text = self.text[:-1]
                 else:
                     self.text += event.unicode
-                self.renderedText = self.renderText(self.text)
+                self.textBox.text = self.text
+                self.textBox.renderedText = self.renderText(self.text)
             
             
