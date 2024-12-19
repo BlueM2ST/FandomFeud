654cd55155ebde2d7212c77e430f9326f8da1994
BlueM2ST <37814898+BlueM2ST@users.noreply.github.com>
2024-12-19 19:09:10 -0400

    textedit now inherits button

--- textEdit.py
+++ textEdit.py
@@ -1,38 +1,32 @@
 
 
-from pygame import draw, MOUSEBUTTONDOWN, KEYDOWN, K_BACKSPACE
-from screenObject import ScreenObject
-from colorRect import ColorRect
-from textBox import TextBox
+from pygame import MOUSEBUTTONDOWN, KEYDOWN, K_BACKSPACE
+from button import Button
 
 
-class TextEdit(ScreenObject):
-    def __init__(self, name:str, posX:int, posY:int, width:int, height:int, text:str="", center:bool=False, padding:int=0):
-        super().__init__(name, posX, posY)
-        self.size = self.toVector((width, height))
-        self.colorInactive = (100, 100, 100)
-        self.colorActive = (150, 150, 150)
-        self.colorCurrent = self.colorInactive
+class TextEdit(Button):
+    def __init__(self, name:str, posX:int, posY:int, width:int, height:int, colorInactive:tuple, colorActive:tuple, text:str="", connection:list=[], disabled:bool=False, centerText:bool=False):
+        super().__init__(name, posX, posY, width, height, text, colorInactive, connection, disabled, centerText)
+        self.colorInactive = colorInactive
+        self.colorActive = colorActive
         self.active = False
-        self.text = text
-        self.rect = self.addChild(ColorRect(self.name + "rect", self.pos.x, self.pos.y, self.size.x, self.size.y, self.colorInactive))
-        self.textBox = self.addChild(TextBox(self.name + "text", self.pos.x, self.pos.y, self.text))
 
     def handleEvent(self, event):
         if event.type == MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
+                print("active")
                 self.active = True
-                self.colorCurrent = self.colorActive
+                self.color = self.colorActive
             else:
                 self.active = False
-                self.colorCurrent = self.colorInactive
+                self.color = self.colorInactive
         elif event.type == KEYDOWN:
             if self.active:
                 if event.key == K_BACKSPACE:
-                    self.text = self.text[:-1]
+                    self.textBox.text = self.textBox.text[:-1]
                 else:
-                    self.text += event.unicode
-                self.textBox.text = self.text
-                self.textBox.renderedText = self.renderText(self.text)
+                    self.textBox.text += event.unicode
+                print(self.textBox.text)
+                self.textBox.renderedText = self.textBox.renderText()
             
             
