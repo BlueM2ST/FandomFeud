a9788167dcbc6aeb8978f1950654757d5bd6fe00
BlueM2ST <37814898+BlueM2ST@users.noreply.github.com>
2024-12-18 22:59:51 -0400

    added audioplayer class

---
+++ audioPlayer.py
@@ -0,0 +1,14 @@
+
+from pygame import mixer
+from screenObject import ScreenObject
+
+mixer.init()
+
+class AudioPlayer(ScreenObject):
+    def __init__(self, name:str, posX:int=0, posY:int=0):
+        super().__init__(name, posX, posY)
+        self.audio = None
+
+    def play(self, filePath:str):
+        self.audio = mixer.Sound(filePath)
+        self.audio.play()
